9fbe24298feb7636d6081c6d9d67433228ec11f9
Evgeny Mozhaev <mozhaevevgeny@gmail.com>
2019-02-19 13:13:43 +0300

    Extract countries and regions

--- app.py
+++ app.py
@@ -9,16 +9,24 @@ from natasha import (
     NamesExtractor,
     AddressExtractor,
     DatesExtractor,
-    MoneyExtractor
+    MoneyExtractor,
+    LocationExtractor
 )
 
 app = Flask(__name__)
 
+# todo: LocationExtractor работает плохо, его надо настравиать,
+# todo: но он умеет находить города, страны и регионы.
+# todo: AddressExtractor лучше находит и представляет города,
+# todo: но не находит страны и регионы. Он находит улицы и дома,
+# todo: нужно исключить их из выдачи и объединить результаты с
+# todo: результатами LocationExtractor-а.
 
 names_extractor = NamesExtractor()
 address_extractor = AddressExtractor()
 dates_extractor = DatesExtractor()
 money_extractor = MoneyExtractor()
+location_extractor = LocationExtractor()
 
 
 def find_named_entities(ner_extractor, text):
@@ -43,11 +51,13 @@ def get_response(text):
     addresses = find_named_entities(address_extractor, text)
     dates = find_named_entities(dates_extractor, text)  # todo: разобраться с parts
     money = find_named_entities(money_extractor, text)
+    locations = find_named_entities(location_extractor, text)
     response = {
         'names': names,
         'addresses': addresses,
         'dates': dates,
         'money': money,
+        'locations': locations,
         'text': text
     }
     return response
